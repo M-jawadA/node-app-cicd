352ceefb9a9c2067c25e300c0787b7234db26f7b
m-jawadA <jawadah303@mail.com>
2022-10-02 20:51:23 +0500

    do some changes to be apeared

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.port || 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello From node CICD ')
+  res.send('Hello From node CICD update ')
 })
 
 app.listen(port, () => {
